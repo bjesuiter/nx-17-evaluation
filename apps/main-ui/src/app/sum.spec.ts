e0be88577e13db942cc4fff9728c00a2b0a8c0e4
Benjamin Jesuiter <benjamin.jesuiter@internett.de>
2024-01-16 18:24:04 +0100

    add small function for vitest to test vitest setup

---
+++ apps/main-ui/src/app/sum.spec.ts
@@ -0,0 +1,21 @@
+import { sum } from "./sum";
+import { describe, expect, it } from "vitest";
+
+describe("sum", () => {
+  it("should return the sum of two numbers", () => {
+    const result = sum(2, 3);
+    expect(result).toEqual(5);
+  });
+
+  it("should return 0 when both numbers are 0", () => {
+    const result = sum(0, 0);
+    expect(result).toEqual(0);
+  });
+
+  it("should return the same number when one of the numbers is 0", () => {
+    const result1 = sum(5, 0);
+    const result2 = sum(0, 5);
+    expect(result1).toEqual(5);
+    expect(result2).toEqual(5);
+  });
+});
