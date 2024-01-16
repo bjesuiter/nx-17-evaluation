0bfffba1df5b8921c928e2906aa891bb3ebbf438
Benjamin Jesuiter <benjamin.jesuiter@internett.de>
2024-01-16 18:18:09 +0100

    add second test

--- apps/main-ui-e2e/src/example.spec.ts
+++ apps/main-ui-e2e/src/example.spec.ts
@@ -1,8 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test("has title", async ({ page }) => {
+  await page.goto("/");
 
   // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  expect(await page.locator("h1").innerText()).toContain("Welcome");
+});
+
+test("has main-ui title", async ({ page }) => {
+  await page.goto("/");
+
+  // Expect h1 to contain a substring.
+  expect(await page.locator("h1").innerText()).toContain("main-ui");
 });
