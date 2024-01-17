ded6729fc9b2a48a57e826c1cb32dece0fca07ca
Benjamin Jesuiter <benjamin.jesuiter@internett.de>
2024-01-17 09:34:17 +0100

    add extra run target for testing vite in browser

--- apps/main-ui/vite.config.browser.ts
+++ apps/main-ui/vite.config.browser.ts
@@ -12,14 +12,14 @@ export default defineConfig(({ mode }) => {
       setupFiles: ["src/test-setup.ts"],
       include: ["**/*.spec.ts"],
       reporters: ["default"],
-      environment: "jsdom", // # can be used to run tests in jsdom
+      // environment: "jsdom", // # can be used to run tests in jsdom
       // alternative: run unit tests in browser via playwright!
-      // browser: {
-      //   enabled: true,
-      //   name: "chromium",
-      //   headless: true, // set to true in CI
-      //   provider: "playwright",
-      // },
+      browser: {
+        enabled: true,
+        name: "chromium",
+        headless: true, // set to true in CI
+        provider: "playwright",
+      },
     },
     define: {
       "import.meta.vitest": mode !== "production",
