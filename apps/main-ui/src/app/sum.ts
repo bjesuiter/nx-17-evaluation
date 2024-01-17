bbff2da883ccfa6b76b1430b453386835dbf8d51
Benjamin Jesuiter <benjamin.jesuiter@internett.de>
2024-01-17 10:32:09 +0100

    add sum2 function

--- apps/main-ui/src/app/sum.ts
+++ apps/main-ui/src/app/sum.ts
@@ -1,3 +1,7 @@
 export function sum(...numbers: number[]): number {
   return numbers.reduce((sum, num) => sum + num, 0);
 }
+
+export function sum2(num1: number, num2: number): number {
+  return num1 + num2;
+}
