573e753fb04f378cc16e03b81db7a62bca68fdba
Benjamin Jesuiter <benjamin.jesuiter@internett.de>
2024-01-17 16:43:06 +0100

    fix normal main-ui vitest config (remove angular() plugin, since this kills test detection for sum.spec.ts

--- apps/main-ui/vite.config.angular-comps-wip.ts
+++ apps/main-ui/vite.config.angular-comps-wip.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 
-// import angular from "@analogjs/vite-plugin-angular";
+import angular from "@analogjs/vite-plugin-angular";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    // plugins: [angular()],
+    plugins: [angular()],
     test: {
       globals: true,
       setupFiles: ["src/test-setup.ts"],
--- apps/main-ui/vite.config.browser.ts
+++ apps/main-ui/vite.config.browser.ts
@@ -1,19 +1,17 @@
 /// <reference types="vitest" />
 
-import angular from "@analogjs/vite-plugin-angular";
+// import angular from "@analogjs/vite-plugin-angular";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [angular()],
+    // plugins: [angular()],
     test: {
       globals: true,
       setupFiles: ["src/test-setup.ts"],
       include: ["**/*.spec.ts"],
       reporters: ["default"],
-      // environment: "jsdom", // # can be used to run tests in jsdom
-      // alternative: run unit tests in browser via playwright!
       browser: {
         enabled: true,
         name: "chromium",
